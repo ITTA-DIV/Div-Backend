87c5b97a959fd38fa4703febbaebe3b96904984e
suucong <sophia021210@naver.com>
2023-12-25 18:48:28 +0900

    feat: Modify Event entity fields (#12)

--- SpringBoot-Damoacon/src/main/java/com/damoacon/app/event/entity/Event.java
+++ SpringBoot-Damoacon/src/main/java/com/damoacon/app/event/entity/Event.java
@@ -8,8 +8,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+
 @Entity
-@Table(name="events")
+@Table(name="event")
 @Getter
 @NoArgsConstructor(access= AccessLevel.PROTECTED)
 public class Event {
@@ -22,38 +24,72 @@ public class Event {
     @NotNull
     private String title;
 
-    @Column(name = "date")
-    private String date;
+    @Column(name = "start_date")
+    @NotNull
+    private Timestamp startDate;
+
+    @Column(name = "end_date")
+    @NotNull
+    private Timestamp end_date;
 
     @Column(name = "price")
+    @NotNull
     private String price;
 
     @Column(name = "location")
+    @NotNull
     private String location;
 
     @Column(name = "address")
     private String address;
 
     @Column(name = "host")
+    @NotNull
     private String host;
 
     @Column(name = "link")
+    @NotNull
     private String link;
 
-    @Column(name = "applydate")
-    private String applydate;
+    @Column(name = "apply_start_date")
+    @NotNull
+    private Timestamp applyStartDate;
+
+    @Column(name = "apply_end_date")
+    @NotNull
+    private Timestamp applyEndDate;
 
     @Column(name = "type")
+    @NotNull
     private String type;
 
+    @Column(name = "thumbnail")
+    @NotNull
+    private String thumbnail;
+
+    @Column(name = "is_permit")
+    @NotNull
+    private int is_permit;
+
     @ManyToOne
-    @JoinColumn(name="category_id")
-    private Category cate;
+    @MapsId("category_id")
+    private Category category;
 
     @Builder
-    public Event (String title,String date,String price,String location,String address,String host,String link){
-        this.title=title;
-        this.date=date;
-        this.price=price;
+    public Event(String title, Timestamp startDate, Timestamp endDate, String price, String location, String address, String host, String link, Timestamp applyStartDate, Timestamp applyEndDate, String type, String thumbnail, int isPermit, Category category) {
+        this.title = title;
+        this.startDate = startDate;
+        this.end_date = endDate;
+        this.price = price;
+        this.location = location;
+        this.address = address;
+        this.host = host;
+        this.link = link;
+        this.applyStartDate = applyStartDate;
+        this.applyEndDate = applyEndDate;
+        this.type = type;
+        this.thumbnail = thumbnail;
+        this.is_permit = isPermit;
+        this.category = category;
     }
 }
