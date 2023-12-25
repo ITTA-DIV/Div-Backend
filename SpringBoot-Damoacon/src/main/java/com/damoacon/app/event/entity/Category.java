b571641bcf1736228c1a04878ffb20c6f944d5b5
suucong <sophia021210@naver.com>
2023-12-25 18:13:30 +0900

    feat: Modify spacing in Category entity (#12)

--- SpringBoot-Damoacon/src/main/java/com/damoacon/app/event/entity/Category.java
+++ SpringBoot-Damoacon/src/main/java/com/damoacon/app/event/entity/Category.java
@@ -1,4 +1,5 @@
 package com.damoacon.app.event.entity;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -16,11 +17,11 @@ public class Category {
     @Column(name = "category_id")
     private Long id;
 
-    @Column(nullable = false,name = "category_name")
+    @Column(nullable = false, name = "category_name")
     private String category_name;
 
     @Builder
     public Category(String category_name){
-        this.category_name=category_name;
+        this.category_name = category_name;
     }
 }
