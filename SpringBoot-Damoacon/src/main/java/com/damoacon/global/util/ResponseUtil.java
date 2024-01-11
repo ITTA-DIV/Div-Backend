af22a7f3ab4f853f8022dd89652ea95564fe5edc
suucong <sophia021210@naver.com>
2024-01-11 16:59:37 +0900

    feat: ResponseUtil 추가 (#13)

---
+++ SpringBoot-Damoacon/src/main/java/com/damoacon/global/util/ResponseUtil.java
@@ -0,0 +1,37 @@
+package com.damoacon.global.util;
+
+import com.damoacon.global.common.ApiDataResponseDto;
+import com.damoacon.global.common.ApiResponseDto;
+import com.damoacon.global.constant.ErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class ResponseUtil {
+    private final ObjectMapper mapper;
+
+    public void setResponse(HttpServletResponse response, ErrorCode code, int status) throws IOException {
+        ApiResponseDto dto = ApiResponseDto.of(true, code);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.setStatus(status);
+        response.getWriter().write(mapper.writeValueAsString(dto));
+    }
+
+    public void setDataResponse(HttpServletResponse response, int status, Object data) throws IOException {
+        ApiDataResponseDto dto = ApiDataResponseDto.of(data);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.setStatus(status);
+        response.getWriter().write(mapper.writeValueAsString(dto));
+    }
+}
