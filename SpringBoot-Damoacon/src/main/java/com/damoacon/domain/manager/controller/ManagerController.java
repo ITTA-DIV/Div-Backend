1962ee744d08a0949865cd8da70be3a49bfd7cab
julia-heo <100216331+julia-heo@users.noreply.github.com>
2024-01-17 01:36:34 +0900

    fix: 응답 형식 통일

--- SpringBoot-Damoacon/src/main/java/com/damoacon/domain/manager/controller/ManagerController.java
+++ SpringBoot-Damoacon/src/main/java/com/damoacon/domain/manager/controller/ManagerController.java
@@ -2,13 +2,12 @@ package com.damoacon.domain.manager.controller;
 
 import com.damoacon.domain.manager.dto.EventCreateDto;
 import com.damoacon.domain.manager.dto.EventResponseDto;
-import com.damoacon.domain.manager.dto.ResponseDto;
-import com.damoacon.domain.manager.service.ManagerService;
+import com.damoacon.domain.manager.service.ManagerServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.damoacon.global.common.ApiDataResponseDto;
 
 import java.util.List;
 
@@ -16,32 +15,43 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/manager")
 public class ManagerController {
-    private final ManagerService managerService;
+    private final ManagerServiceImpl managerService;
 
     @PostMapping
-    public ResponseEntity<ResponseDto<Object>> save(@RequestBody EventCreateDto requestDto){
-        try {
-            EventResponseDto newEvent = managerService.eventCreate(requestDto);
-            return ResponseEntity.ok().body(ResponseDto.response(HttpStatus.CREATED, "이벤트 신청 생성 성공",newEvent));
+    public ApiDataResponseDto<EventResponseDto> save(@RequestBody EventCreateDto requestDto){
+        try{
+            EventResponseDto newEvents = managerService.eventCreate(requestDto);
+            return ApiDataResponseDto.of(newEvents);
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(ResponseDto.response(HttpStatus.BAD_REQUEST, "이벤트 신청 생성 실패"));
-        }
-    }
-    @GetMapping
-    public ResponseEntity<ResponseDto<Object>> applyList(){
-        try {
-            return ResponseEntity.ok().body(ResponseDto.response(HttpStatus.OK, "이벤트 신청 목록 조회 성공",managerService.eventApplyList()));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(ResponseDto.response(HttpStatus.BAD_REQUEST, "이벤트 신청 목록 조회 실패"));
+            return ApiDataResponseDto.of(null, "이벤트 신청 생성 실패");
         }
     }
 
-    @PatchMapping("{event_id}")
-    public ResponseEntity<ResponseDto<Object>> changePermit(@PathVariable Long event_id){
+    @GetMapping
+    public ApiDataResponseDto<List<EventResponseDto>> applyList(){
         try {
-            return ResponseEntity.ok().body(ResponseDto.response(HttpStatus.OK, "이벤트 요청 승인",managerService.eventApplyPermit(event_id)));
+            List<EventResponseDto> applyeventlist=managerService.eventApplyList();
+            return ApiDataResponseDto.of(applyeventlist);
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(ResponseDto.response(HttpStatus.BAD_REQUEST, "이벤트 요청 승인 실패"));
+            return ApiDataResponseDto.of(null, "이벤트 신청 목록 조회 실패");
+        }
+    }
+
+//    @PatchMapping("{event_id}")
+//    public ResponseEntity<ResponseDto<Object>> changePermit(@PathVariable Long event_id){
+//        try {
+//            return ResponseEntity.ok().body(ResponseDto.response(HttpStatus.OK, "이벤트 요청 승인",managerService.eventApplyPermit(event_id)));
+//        } catch (Exception e) {
+//            return ResponseEntity.badRequest().body(ResponseDto.response(HttpStatus.BAD_REQUEST, "이벤트 요청 승인 실패"));
+//        }
+//    }
+    @PatchMapping("{event_id}")
+    public ApiDataResponseDto<EventResponseDto> changePermit(@PathVariable Long event_id){
+        try {
+            EventResponseDto changeApplyEvent = managerService.eventApplyPermit(event_id);
+            return ApiDataResponseDto.of(changeApplyEvent);
+        } catch (Exception e) {
+            return ApiDataResponseDto.of(null, "이벤트 요청 승인 실패");
         }
     }
 }
