4d2e0affeba6bb849a28a3a2e2eab691c790516f
suucong <sophia021210@naver.com>
2024-01-24 21:06:46 +0900

    refactor: 사용하지 않는 domain 제거 (#63)

--- src/main/java/com/damoacon/domain/survey/repository/AnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.damoacon.domain.survey.repository;
-
-import com.damoacon.domain.survey.entity.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-}
